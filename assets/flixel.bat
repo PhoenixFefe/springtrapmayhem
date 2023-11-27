e6bee1f88d7d7bf98e57bcfd1c4e3eb1ae750867
PhoenixFefe <151916015+PhoenixFefe@users.noreply.github.com>
2023-11-27 09:40:54 +0100

    Add files via upload

---
+++ assets/flixel.bat
@@ -0,0 +1,15 @@
+haxelib install flixel
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+haxelb install linc_luajit
+haxelib install sscript
+haxelib install hxcodec
+haxelib install discord_rpc
+haxelib install tjson
+
+pause
